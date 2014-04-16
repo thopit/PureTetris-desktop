6066693a7f4818d4a1c79f12e30fc16012510492
Thomas Opitz <opitz-t@web.de>
2014-04-16 18:55:44 +0200

    Setup of project

---
+++ PureTetris-desktop/src/de/thomas/pure_tetris/Main.java
@@ -0,0 +1,39 @@
+// Copyright (c) 2014  Thomas Opitz
+//  
+// This file is part of PureTetris.
+//
+// PureTetris is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by 
+// the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// PureTetris is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with PureTetris.
+// If not, see http://www.gnu.org/licenses/.
+//
+//  e-mail : opitz-t@web.de
+//
+// *************************************************************************
+
+package de.thomas.pure_tetris;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Tetris";
+		cfg.width = 480;
+		cfg.height = 800;
+		
+		cfg.addIcon("O.png", FileType.Internal);
+		cfg.addIcon("OMiddle.png", FileType.Internal);
+		cfg.addIcon("OSmall.png", FileType.Internal);
+		
+		new LwjglApplication(new Tetris(), cfg);
+	}
+}
